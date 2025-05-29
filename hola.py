7686026b5c32b390520c58b7cd781a3637c85121
Jona669 <mayjona81@gmail.com>
2025-05-29 15:04:29 -0400

    a

--- hola.py
+++ hola.py
@@ -60,3 +60,29 @@
 #     print("es de audax")
 # else: print("color invalido")
 
+
+#atletas de salto alto
+#ingrese cantidad de atletas que paticiparan
+#cada atletadebe hacer un alto alto en el rango de 1.55 mt y 3.78 mt
+# los atletas bajo 1.55 no califican entre 1.56 y 2 bronce, entre 2.1 y 3 plata, entre 3.1 y mas oro
+#poner la altura maxima lograda
+
+atle=int(input("ingrese la cantidad de atletas: "))
+record=0
+import random
+
+for atleta in range(atle):
+    salto=round (random.uniform(1, 3.78), 2)
+    if salto>record:
+        record=salto
+    print(f"El atleta {atleta+1} salto {salto} mts")
+
+    if salto<=1.55:
+        print("No clasefica")
+    elif salto>=1.56 and salto<=2.0:
+        print("A ganado el bronce")
+    elif salto>=2.1 and salto<=3.0:
+        print("A ganado la plata")
+    else:
+        print("A ganado el oro")
+    print("El record acual es ", record)
---
+++ repaso.py
@@ -0,0 +1,7 @@
+#atletas de salto alto
+#ingrese cantidad de atletas que paticiparan
+#cada atletadebe hacer un alto alto en el rango de 1.55 mt y 3.78 mt
+# los atletas bajo 1.55 no califican entre 1.56 y 2 bronce, entre 2.1 y 3 plata, entre 3.1 y mas oro
+
+atle=int(input(print"ingrese la cantidad de atletas"))
+salto=random.uniform(1.56, 3.78)
